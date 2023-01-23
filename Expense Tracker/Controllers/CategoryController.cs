21ff810d93728a5fef2237ecc606af94900d24c4
Linlin Deng <linlin.deng@icemedia.com.au>
2023-01-23 11:31:29 +1000

    update2

--- Expense Tracker/Controllers/CategoryController.cs
+++ Expense Tracker/Controllers/CategoryController.cs
@@ -24,26 +24,8 @@ namespace Expense_Tracker.Controllers
               return View(await _context.Categories.ToListAsync());
         }
 
-        // GET: Category/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
-
         // GET: Category/AddOrEdit
-        public IActionResult AddOrEdit(int id=0)
+        public IActionResult AddOrEdit(int id = 0)
         {
             if(id == 0)
                 return View(new Category());
@@ -88,6 +70,7 @@ namespace Expense_Tracker.Controllers
         }
         */
 
+        /*
         // POST: Category/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -122,24 +105,7 @@ namespace Expense_Tracker.Controllers
             }
             return View(category);
         }
-
-        // GET: Category/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
+       */
 
         // POST: Category/Delete/5
         [HttpPost, ActionName("Delete")]
@@ -160,9 +126,5 @@ namespace Expense_Tracker.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        private bool CategoryExists(int id)
-        {
-          return _context.Categories.Any(e => e.CategoryId == id);
-        }
     }
 }
