8e547ea9d97c9993264fbb57e0443995d213ab6f
Linlin Deng <linlin.deng@icemedia.com.au>
2023-01-23 16:08:30 +1000

    update4

--- Expense Tracker/Controllers/TransactionController.cs
+++ Expense Tracker/Controllers/TransactionController.cs
@@ -28,7 +28,7 @@ namespace Expense_Tracker.Controllers
         // GET: Transaction/AddOrEdit
         public IActionResult AddOrEdit()
         {
-            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId");
+            PopulateCategories();
             return View(new Transaction());
         }
 
@@ -45,7 +45,7 @@ namespace Expense_Tracker.Controllers
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", transaction.CategoryId);
+            PopulateCategories();
             return View(transaction);
         }
 
@@ -67,6 +67,15 @@ namespace Expense_Tracker.Controllers
             
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        [NonAction]
+        public void PopulateCategories()
+        {
+            var CategoryCollection = _context.Categories.ToList();
+            Category DefaultCategory = new Category() { CategoryId = 0, Title = "Choose a Category" };
+            CategoryCollection.Insert(0, DefaultCategory);
+            ViewBag.Categories = CategoryCollection;
         }
     }
 }
--- Expense Tracker/Models/Transaction.cs
+++ Expense Tracker/Models/Transaction.cs
@@ -12,7 +12,7 @@ namespace Expense_Tracker.Models
         [Key]
         public int TransactionId { get; set; }
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public Category? Category { get; set; }
         public int Amount { get; set; }
         [Column(TypeName = "nvarchar(75)")]
         public string? Note { get; set; }
